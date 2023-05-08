7a38d94d06af801d20e3581ccaee9010c7580c6d
Morteza REZAEI <64468530+Morteza-Rezaei@users.noreply.github.com>
2023-05-08 23:26:59 +0300

    final-commit

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,52 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
-
-import 'data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
-  State<Quiz> createState() => _QuizState();
+  State<Quiz> createState() {
+    return _QuizState();
+  }
 }
 
 class _QuizState extends State<Quiz> {
   List<String> _selectedAnswers = [];
-
   var _activeScreen = 'start-screen';
 
   void _switchScreen() {
     setState(() {
-      _activeScreen = 'question-screen';
+      _activeScreen = 'questions-screen';
     });
   }
 
   void _chooseAnswer(String answer) {
-    _selectedAnswers.length;
+    _selectedAnswers.add(answer);
+
     if (_selectedAnswers.length == questions.length) {
       setState(() {
-        _activeScreen = 'result-screen';
+        _activeScreen = 'results-screen';
       });
     }
   }
 
-  void _restartQuiz() {
+  void restartQuiz() {
     setState(() {
-      _activeScreen = 'question-screen';
+      _activeScreen = 'questions-screen';
+      _selectedAnswers = [];
     });
   }
 
   @override
   Widget build(context) {
     Widget screenWidget = StartScreen(_switchScreen);
-    if (_activeScreen == 'question-screen') {
-      screenWidget = QuestionsScreen();
+
+    if (_activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: _chooseAnswer,
+      );
     }
 
-    if (_activeScreen == 'result-screen') {
-      screenWidget = ResultsScreen();
+    if (_activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: _selectedAnswers,
+        onRestart: restartQuiz,
+      );
     }
 
     return MaterialApp(
@@ -58,8 +66,8 @@ class _QuizState extends State<Quiz> {
                 Color.fromRGBO(164, 255, 164, 1),
                 Color.fromRGBO(255, 255, 164, 1),
               ],
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           child: screenWidget,
